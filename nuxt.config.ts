cd2b18cebf32e8f744ef499ea95241ff5c12a193
meijin <i8858garden@gmail.com>
2021-01-15 01:12:10 +0000

    pre install firebase-tools to global

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,10 +29,10 @@ const config: NuxtConfig = {
         )
         try {
           session.database().goOffline()
-        } catch (e) {}
+        } catch (e) { }
         try {
           session.firestore().terminate()
-        } catch (e) {}
+        } catch (e) { }
       },
     },
   },
@@ -48,14 +48,14 @@ const config: NuxtConfig = {
   firebase: {
     lazy: false,
     config: {
-      apiKey: `${process.env.FIREBASE_API_KEY}`,
-      authDomain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
-      databaseURL: `${process.env.FIREBASE_DATABASE_URL}`,
-      projectId: `${process.env.FIREBASE_PROJECT_ID}`,
-      storageBucket: `${process.env.FIREBASE_STORAGE_BUCKET}`,
-      messagingSenderId: `${process.env.FIREBASE_MESSAGING_SENDER_ID}`,
-      appId: `${process.env.FIREBASE_APP_ID}`,
-      measurementId: `${process.env.FIREBASE_MEASUREMENT_ID}`,
+      apiKey: `${process.env.FIREBASE_APIKEY}`,
+      authDomain: `${process.env.FIREBASE_AUTHDOMAIN}`,
+      databaseURL: `${process.env.FIREBASE_DATABASEURL}`,
+      projectId: `${process.env.FIREBASE_PROJECTID}`,
+      storageBucket: `${process.env.FIREBASE_STORAGEBUCKET}`,
+      messagingSenderId: `${process.env.FIREBASE_MESSAGINGSENDERID}`,
+      appId: `${process.env.FIREBASE_APPID}`,
+      measurementId: `${process.env.FIREBASE_MEASUREMENTID}`,
     },
     onFirebaseHosting: false,
     services: {
